24d2ae73215059cec52f418cd3005375ccc81569
KateCourt <49239607+KateCourt@users.noreply.github.com>
2023-02-03 14:13:40 +0000

    comparison adds user and turn off draft publishing liver scores and comparisons

--- src/api/comparison/controllers/comparison.js
+++ src/api/comparison/controllers/comparison.js
@@ -6,4 +6,16 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::comparison.comparison');
+module.exports = createCoreController('api::comparison.comparison', ({ strapi }) => ({
+    async create(ctx, next) {
+        const user = ctx.state.user
+
+        const data = {
+            ...ctx.request.body.data,
+            users_permissions_user: user
+        }
+        console.log(data)
+        const response = await strapi.entityService.create('api::comparison.comparison', {data})
+        return response
+    }
+}));
