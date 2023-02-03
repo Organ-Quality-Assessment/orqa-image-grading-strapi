ef53d5dfab2f6f1bbe5f19046ff8cee96e54ea5d
KateCourt <49239607+KateCourt@users.noreply.github.com>
2023-02-03 11:38:22 +0000

    get user from token when creating new liver score

--- src/api/liver-score/controllers/liver-score.js
+++ src/api/liver-score/controllers/liver-score.js
@@ -6,4 +6,17 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::liver-score.liver-score');
+module.exports = createCoreController('api::liver-score.liver-score', ({ strapi }) => ({
+    async create(ctx, next) {
+        const user = ctx.state.user
+
+        const data = {
+            ...ctx.request.body.data,
+            users_permissions_user: user
+        }
+        console.log(data)
+        const response = await strapi.entityService.create('api::liver-score.liver-score', {data})
+        // const response = await super.create(ctx)
+        return response
+    }
+}));
