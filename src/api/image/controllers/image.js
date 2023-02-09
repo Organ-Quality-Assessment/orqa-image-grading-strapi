6d8a0fcaa4aab7b790a7d1d31c7d981f45853e7c
KateCourt <49239607+KateCourt@users.noreply.github.com>
2023-02-09 13:54:45 +0000

    create generic score collection and implement images to grade endpoint

--- src/api/image/controllers/image.js
+++ src/api/image/controllers/image.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var _ = require('lodash');
 
 
 /**
@@ -10,12 +11,12 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::image.image', ({strapi}) => ({
     imagesToGrade: async (ctx, next) => {
-        const totalImages = 9
+        const totalImages = 2
         // get images user wants to grade
         let {organs} = ctx.request.query
         organs = organs.split(',')
         
-        // split images into the selected organs
+        // split images into the selected organs - currently fairly crude way of allocating
         const numberOrgans = organs.length
         const numberImagesPerOrgan = []
  
@@ -25,28 +26,67 @@ if (totalImages % numberOrgans == 0) {
     const imagesPerOrgan = totalImages/numberOrgans
     // populate list
     for (var i=0; i < (numberOrgans); i++) {
-        console.log('here')
-        console.log(imagesPerOrgan)
         numberImagesPerOrgan.push(imagesPerOrgan)
     }
-    console.log(numberImagesPerOrgan)
+    
 } else {
     // images are not going to split evenly
     var remainder = totalImages%numberOrgans;
     var wholes = Math.floor(totalImages / numberOrgans);
- var output = ''
-
-    for (var i = 0; i < (wholes - 1); i++) {
-        // numberImagesPerOrgan.push(output)
-      output+= numberOrgans + ', ';
+    
+    for (var i = 0; i < (numberOrgans); i++) {
+        numberImagesPerOrgan.push(wholes)
     }
     
-     output += (numberOrgans + remainder);
-    
-    console.log(output)
+    numberImagesPerOrgan[numberImagesPerOrgan.length-1]= numberImagesPerOrgan[numberImagesPerOrgan.length-1]+remainder
 }
-console.log(numberImagesPerOrgan)
-        return 'test'
+
+// todo ideally shuffle the array
+
+// get number of real images that have the least scores
+const images = await strapi.entityService.findMany('api::image.image', {
+    filters: {
+        real: true
+    },
+    fields: ['id', 'real', 'filename'],
+    populate: { 
+        scores: { count: true},
+        organ: true
+    }
+})
+
+// group by organ
+const imagesPerOrgan = images.reduce((imagesPerOrgan, item) => {
+    const  organ = (imagesPerOrgan[item.organ.organ_type] || []);
+  organ.push(item);
+  imagesPerOrgan[item.organ.organ_type] = organ;
+  return imagesPerOrgan;
+}, {})
+
+// within each organ order by number of scores and get number needed
+var toReturn = []
+
+for (var i=0; i < organs.length; i++) {
+    const numberRequired = numberImagesPerOrgan[i]
+    const organ_name = organs[i]
+
+    var imagesForThatOrgan = imagesPerOrgan[organ_name]
+    imagesForThatOrgan = _.sortBy(imagesForThatOrgan, [function(o) { return o.scores}])
+
+    const imagesToGrade = imagesForThatOrgan.slice(0, numberRequired)
+    console.log(organ_name)
+    console.log(numberRequired)
+    console.log(imagesToGrade)    
+
+    toReturn.push({
+        organ_type: organ_name,
+        images: imagesToGrade
+    })
+}
+
+
+
+        return toReturn
     }
 }
 
--- src/api/score/services/score.js
+++ src/api/score/services/score.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * liver-score service
+ * score service
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::liver-score.liver-score');
+module.exports = createCoreService('api::score.score');
